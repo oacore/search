b5e9e03356f7ca987fb31d9b05b3e571c1c0969e
Jozef Harag <32jojo32@gmail.com>
2020-07-14 12:01:19 +0200

    Move layout closer to the Registration (#6)
    Since the project is not only registration it's better
    to have this layout closer to the registration.

--- modules/registration-layout/index.jsx
+++ modules/registration-layout/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import styles from './styles.module.css'
 
-const Layout = ({ children }) => (
+const RegistrationLayout = ({ children }) => (
   <main className={styles.layout}>
     <div className={styles.content}>{children}</div>
   </main>
 )
 
-export default Layout
+export default RegistrationLayout
