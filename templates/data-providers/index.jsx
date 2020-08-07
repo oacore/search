f9a09106174771c09425d7eb378b203826805332
Viktor Yakubiv <viktor@yakubiv.com>
2020-08-07 17:27:39 +0300

    Remove the 2nd step from the registration (#54)
    The second 'success' step is blended into the success message of
    the input URL validation since it's considered redundant itself.
    
    The submit button caption changed from 'Continue' to 'Done'
    and it resets the form when clicked now.
    
    The field placeholder was updated to better represent
    the input expectations.
    
    All extra code is removed.

--- templates/data-providers/index.jsx
+++ templates/data-providers/index.jsx
@@ -2,20 +2,10 @@ import React from 'react'
 import { TextField, Button, Card } from '@oacore/design'
 
 import styles from './styles.module.css'
-import SuccessMessage from './success-message'
 
 const DataProviderPageTemplate = React.forwardRef(
   (
-    {
-      url,
-      onSubmit,
-      onUrlChange,
-      helperMessage,
-      variant,
-      showSuccessMessage,
-      oaiPmhUrl,
-      isFormValid = false,
-    },
+    { url, onSubmit, onUrlChange, helperMessage, variant, isFormValid = false },
     ref
   ) => {
     const handleSubmit = (event) => {
@@ -25,32 +15,27 @@ const DataProviderPageTemplate = React.forwardRef(
 
     return (
       <Card className={styles.layout}>
-        {showSuccessMessage && <SuccessMessage oaiPmhUrl={oaiPmhUrl} />}
-        {!showSuccessMessage && (
-          <>
-            <Card.Title>Register new data provider</Card.Title>
-            <form ref={ref} className={styles.form} onSubmit={handleSubmit}>
-              <TextField
-                id="data-provider-url"
-                type="url"
-                name="dataProviderUrl"
-                label="Data provider URL"
-                helper={helperMessage}
-                placeholder="For example: oro.open.ac.uk"
-                value={url}
-                onChange={onUrlChange}
-                variant={variant}
-                statusIcon
-                required
-              />
-              <div className={styles.submitSection}>
-                <Button variant="contained" disabled={!isFormValid}>
-                  Continue
-                </Button>
-              </div>
-            </form>
-          </>
-        )}
+        <Card.Title>Register new data provider</Card.Title>
+        <form ref={ref} className={styles.form} onSubmit={handleSubmit}>
+          <TextField
+            id="data-provider-url"
+            type="url"
+            name="dataProviderUrl"
+            label="Data provider URL"
+            helper={helperMessage}
+            placeholder="For example, https://oro.open.ac.uk"
+            value={url}
+            onChange={onUrlChange}
+            variant={variant}
+            statusIcon
+            required
+          />
+          <div className={styles.submitSection}>
+            <Button variant="contained" disabled={!isFormValid}>
+              Done
+            </Button>
+          </div>
+        </form>
       </Card>
     )
   }
