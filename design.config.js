41ce7c7bc48ea9ddde952774b28b685cd66cd1bc
Jozef Harag <32jojo32@gmail.com>
2020-08-11 16:39:29 +0200

    Style result card

--- design.config.js
+++ design.config.js
@@ -1,6 +1,18 @@
 const path = require('path')
 
+const icons = ['office-building']
+
+const iconsRoot = path.join(
+  path.dirname(require.resolve('@mdi/svg/package.json')),
+  './svg'
+)
+
 const config = {
+  icons: {
+    path: iconsRoot,
+    files: icons,
+  },
+
   output: {
     path: path.join(__dirname, 'public/design'),
     publicPath: '/design',
