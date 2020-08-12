6278cd294f07fbc249984c32d1154f4f72947333
Jozef Harag <32jojo32@gmail.com>
2020-08-12 11:25:50 +0200

    Connect to API

--- templates/data-providers-search/index.jsx
+++ templates/data-providers-search/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { Button } from '@oacore/design'
 
 import styles from './styles.module.css'
@@ -7,58 +7,121 @@ import ResultCard from './result-card'
 
 import Search from 'modules/search-layout'
 
-const DataProvidersSearchTemplate = () => (
-  <Search className={styles.searchArea}>
-    <DataProvidersSelect />
-    <Search.ResultStats from={1} to={10} total={9934} />
+// TODO: Fuzzy search via Fuse.js would be better but it's too slow for that
+//       amount of data providers. We should consider migrating to backend
+//       search once apiv3 is released
+const normalize = (string) =>
+  string.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+const normalizeDataProviders = (dataProviders) =>
+  dataProviders
+    .filter((el) => el.name)
+    .map((el) => ({
+      ...el,
+      normalizedName: normalize(el.name || ''),
+    }))
+
+const SearchResults = ({
+  results,
+  dataProvidersOffset,
+  setDataProvidersOffset,
+}) => {
+  if (!results.length) {
+    return (
+      <Search.Results>
+        <Search.Result>
+          <b>No results found</b>
+        </Search.Result>
+      </Search.Results>
+    )
+  }
+
+  return (
     <Search.Results>
-      <ResultCard
-        repoId={1}
-        title="Aberdeen University Research Archive"
-        homePage="aura.abdn.ac.uk"
-        country="United Kingdom"
-      />
-      <ResultCard
-        repoId={1}
-        title="Aberdeen University Research Archive"
-        homePage="aura.abdn.ac.uk"
-        country="United Kingdom"
-      />
-      <ResultCard
-        repoId={1}
-        title="Aberdeen University Research Archive"
-        homePage="aura.abdn.ac.uk"
-        country="United Kingdom"
-      />
-      <ResultCard
-        repoId={1}
-        title="Aberdeen University Research Archive"
-        homePage="aura.abdn.ac.uk"
-        country="United Kingdom"
-      />
-      <ResultCard
-        repoId={1}
-        title="Aberdeen University Research Archive"
-        homePage="aura.abdn.ac.uk"
-        country="United Kingdom"
-      />
+      {results.slice(0, dataProvidersOffset).map((el) => (
+        <ResultCard
+          key={el.id}
+          repoId={el.id}
+          title={el.name}
+          homePage={el.urlHomepage}
+          country={el.repositoryLocation?.countryName || 'unknown location'}
+        />
+      ))}
+      {dataProvidersOffset < results.length && (
+        <Button
+          onClick={() => setDataProvidersOffset(dataProvidersOffset + 10)}
+          className={styles.loadMore}
+        >
+          Load more data providers
+        </Button>
+      )}
     </Search.Results>
-    <Search.Content>
-      <p>
-        We aggregate research papers from data providers all over the world
-        including institutional and subject repositories and journal publishers.
-        This process, which is also called harvesting, allows us to offer
-        search, text mining and analytical capabilities over not only metadata,
-        but also the full text of the research papers making CORE a unique
-        service in the research community. Our dataset currently contains
-        182,918,912 open access articles, from over tons of thousands journals,
-        collected from over 9,972 repositories and journals around the world.
-      </p>
-      <Button variant="outlined">Become data provider</Button>
-    </Search.Content>
-    <Search.Footer>
-      <Button variant="contained">Add data provider</Button>
-    </Search.Footer>
-  </Search>
-)
+  )
+}
+
+const DataProvidersSearchTemplate = ({ dataProviders }) => {
+  const [query, setQuery] = useState('')
+  const [dataProvidersOffset, setDataProvidersOffset] = useState(10)
+  const dataProvidersSearch = useMemo(
+    () => normalizeDataProviders(dataProviders),
+    [dataProviders]
+  )
+  const [results, setResults] = useState(dataProvidersSearch.slice(0, 10))
+
+  useEffect(() => {
+    setDataProvidersOffset(10)
+
+    if (query === '') setResults(normalizeDataProviders(dataProviders))
+    else {
+      setResults(
+        dataProvidersSearch.filter(
+          (el) =>
+            el.name?.toLowerCase().search(query) !== -1 ||
+            el.normalizedName.toLowerCase().search(query) !== -1
+        )
+      )
+    }
+  }, [query])
+
+  return (
+    <>
+      <DataProvidersSelect
+        onQueryChanged={setQuery}
+        dataProvidersSearch={dataProvidersSearch}
+      />
+      <Search className={styles.searchArea}>
+        {Boolean(results.length) && (
+          <Search.ResultStats
+            from={1}
+            to={Math.min(dataProvidersOffset, results.length)}
+            total={results.length}
+          />
+        )}
+        <SearchResults
+          dataProvidersOffset={dataProvidersOffset}
+          results={results}
+          setDataProvidersOffset={setDataProvidersOffset}
+        />
+
+        <Search.Content>
+          <p>
+            We aggregate research papers from data providers all over the world
+            including institutional and subject repositories and journal
+            publishers. This process, which is also called harvesting, allows us
+            to offer search, text mining and analytical capabilities over not
+            only metadata, but also the full text of the research papers making
+            CORE a unique service in the research community. Our dataset
+            currently contains 182,918,912 open access articles, from over tons
+            of thousands journals, collected from over {dataProviders.length}{' '}
+            repositories and journals around the world.
+          </p>
+          <Button variant="outlined">Become data provider</Button>
+        </Search.Content>
+        <Search.Footer>
+          <Button variant="contained">Add data provider</Button>
+        </Search.Footer>
+      </Search>
+    </>
+  )
+}
 export default DataProvidersSearchTemplate
--- templates/data-providers-search/search.jsx
+++ templates/data-providers-search/search.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { throttle } from 'throttle-debounce'
 
 import styles from './styles.module.css'
 
 import AppBarItem from 'modules/app-bar-item'
 import Select from 'modules/app-bar-select'
 
-const options = [
-  { id: 1, icon: '#magnify', value: 'Option A' },
-  { id: 2, icon: '#magnify', value: 'Option B' },
-  { id: 3, icon: '#magnify', value: 'Option C' },
-  { id: 4, icon: '#magnify', value: 'Option D' },
-  { id: 5, icon: '#magnify', value: 'Option E' },
-  { id: 6, icon: '#magnify', value: 'Option F' },
-]
+const DataProvidersSelect = ({ dataProvidersSearch, onQueryChanged }) => {
+  const [suggestions, setSuggestions] = React.useState([])
+  const [value, setValue] = React.useState('')
 
-const DataProvidersSelect = () => {
-  const [suggestions, setSuggestions] = React.useState(options)
-  const [value, setValue] = React.useState(null)
+  const search = useCallback(
+    throttle(500, false, (searchTerm) => {
+      setSuggestions(
+        dataProvidersSearch
+          .filter(
+            (el) =>
+              el.name?.search(searchTerm) !== -1 ||
+              el.normalizedName.search(searchTerm) !== -1
+          )
+          .slice(0, 10)
+      )
+    }),
+    []
+  )
 
-  const handleOnChange = () => {
-    // trigger search here
+  const handleOnChange = (data) => {
+    onQueryChanged(data.value)
+    search(data.value)
   }
 
   const handleOnInput = (data) => {
+    setValue(data.value)
+
     // if id doesn't exists it means user type own text
     // and didn't use suggestion
-    if (!data.id) {
-      setSuggestions(
-        options.slice(
-          0,
-          Math.max(0, options.length - (data.value?.length || 0))
-        )
-      )
-    }
-
-    setValue(data.value)
+    if (!data.id) search(data.value)
   }
 
   return (
@@ -48,11 +49,10 @@ const DataProvidersSelect = () => {
         onInput={handleOnInput}
         placeholder="e.g. repository or journal name"
         prependIcon="#magnify"
-        className={styles.search}
       >
         {suggestions.map((el) => (
-          <Select.Option key={el.id} id={el.id} value={el.value} icon={el.icon}>
-            {el.value}
+          <Select.Option key={el.id} id={el.id} value={el.name} icon="#magnify">
+            {el.name}
           </Select.Option>
         ))}
       </Select>
