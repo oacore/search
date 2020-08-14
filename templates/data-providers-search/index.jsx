5758b03dd930e436a59cf0b0d1575400b1d92720
Jozef Harag <32jojo32@gmail.com>
2020-08-14 15:07:22 +0200

    Move state logic one level up

--- templates/data-providers-search/index.jsx
+++ templates/data-providers-search/index.jsx
@@ -18,20 +18,6 @@ import RepositoriesMap from 'modules/repositories-map'
 const useIsomorphicLayoutEffect =
   typeof window !== 'undefined' ? useLayoutEffect : useEffect
 
-// TODO: Fuzzy search via Fuse.js would be better but it's too slow for that
-//       amount of data providers. We should consider migrating to backend
-//       search once apiv3 is released
-const normalize = (string) =>
-  string.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-
-const normalizeDataProviders = (dataProviders) =>
-  dataProviders
-    .filter((el) => el.name)
-    .map((el) => ({
-      ...el,
-      normalizedName: normalize(el.name || ''),
-    }))
-
 const SearchResults = ({
   results,
   dataProvidersOffset,
@@ -79,84 +65,60 @@ const SearchResults = ({
 }
 
 const DataProvidersSearchTemplate = React.memo(
-  ({ dataProviders, queryParam, dataProvidersOffsetParam, setUrlParams }) => {
-    const [query, setQuery] = useState(queryParam || '')
-    const [dataProvidersOffset, setDataProvidersOffset] = useState(
-      dataProvidersOffsetParam || 10
-    )
-    const dataProvidersSearch = useMemo(
-      () => normalizeDataProviders(dataProviders),
-      [dataProviders]
-    )
-    const searchDataProviders = useCallback(
-      (searchTerm) =>
-        dataProvidersSearch.filter(
-          (el) =>
-            el.name?.toLowerCase().search(searchTerm.toLowerCase()) !== -1 ||
-            el.normalizedName.toLowerCase().search(searchTerm.toLowerCase()) !==
-              -1
-        ),
-      [dataProvidersSearch]
-    )
-    const [results, setResults] = useState(searchDataProviders(query))
-
-    useEffect(() => {
-      setDataProvidersOffset(10)
-      if (query === '') setResults(normalizeDataProviders(dataProviders))
-      else setResults(searchDataProviders(query))
-    }, [query])
-
-    useEffect(() => {
-      setUrlParams({ query, size: dataProvidersOffset })
-    }, [query, dataProvidersOffset])
-
-    return (
-      <>
-        <DataProvidersSelect
-          onQueryChanged={setQuery}
-          searchDataProviders={searchDataProviders}
-          initQuery={query}
-        />
-        <Search className={styles.searchArea}>
-          {Boolean(results.length) && (
-            <Search.ResultStats
-              from={1}
-              to={Math.min(dataProvidersOffset, results.length)}
-              total={results.length}
-            />
-          )}
-          <SearchResults
-            dataProvidersOffset={dataProvidersOffset}
-            results={results}
-            setDataProvidersOffset={setDataProvidersOffset}
+  ({
+    dataProviders,
+    query,
+    dataProvidersOffset,
+    results,
+    setDataProvidersOffset,
+    searchDataProviders,
+    setQuery,
+  }) => (
+    <>
+      <DataProvidersSelect
+        onQueryChanged={setQuery}
+        searchDataProviders={searchDataProviders}
+        initQuery={query}
+      />
+      <Search className={styles.searchArea}>
+        {Boolean(results.length) && (
+          <Search.ResultStats
+            from={1}
+            to={Math.min(dataProvidersOffset, results.length)}
+            total={results.length}
           />
+        )}
+        <SearchResults
+          dataProvidersOffset={dataProvidersOffset}
+          results={results}
+          setDataProvidersOffset={setDataProvidersOffset}
+        />
 
-          <Search.Content>
-            <RepositoriesMap
-              dataProviders={
-                query === '' ? results : results.slice(0, dataProvidersOffset)
-              }
-            />
-            <p>
-              We aggregate research papers from data providers all over the
-              world including institutional and subject repositories and journal
-              publishers. This process, which is also called harvesting, allows
-              us to offer search, text mining and analytical capabilities over
-              not only metadata, but also the full text of the research papers
-              making CORE a unique service in the research community. Our
-              dataset currently contains 182,918,912 open access articles, from
-              over tons of thousands journals, collected from over{' '}
-              {dataProviders.length} repositories and journals around the world.
-            </p>
-            <Button variant="outlined">Become data provider</Button>
-          </Search.Content>
-          <Search.Footer>
-            <Button variant="contained">Add data provider</Button>
-          </Search.Footer>
-        </Search>
-      </>
-    )
-  }
+        <Search.Content>
+          <RepositoriesMap
+            dataProviders={
+              query === '' ? results : results.slice(0, dataProvidersOffset)
+            }
+          />
+          <p>
+            We aggregate research papers from data providers all over the world
+            including institutional and subject repositories and journal
+            publishers. This process, which is also called harvesting, allows us
+            to offer search, text mining and analytical capabilities over not
+            only metadata, but also the full text of the research papers making
+            CORE a unique service in the research community. Our dataset
+            currently contains 182,918,912 open access articles, from over tons
+            of thousands journals, collected from over {dataProviders.length}{' '}
+            repositories and journals around the world.
+          </p>
+          <Button variant="outlined">Become data provider</Button>
+        </Search.Content>
+        <Search.Footer>
+          <Button variant="contained">Add data provider</Button>
+        </Search.Footer>
+      </Search>
+    </>
+  )
 )
 
 export default DataProvidersSearchTemplate
