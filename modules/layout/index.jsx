4eda27d557b4de59ea513a5726fa92eb28af09c3
Jozef Harag <32jojo32@gmail.com>
2020-09-28 11:47:53 +0200

    Add footer

--- modules/layout/index.jsx
+++ modules/layout/index.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import Container from './container'
 import AppBar from './app-bar'
 import styles from './styles.module.css'
+import Footer from './footer'
 
 const Layout = ({ children }) => (
   <Container>
     <AppBar />
     <main className={styles.main}>{children}</main>
+    <Footer />
   </Container>
 )
 
