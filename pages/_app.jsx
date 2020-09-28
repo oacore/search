c786aedb571e0f6f8dd2f2ad2a540160b59ff141
Jozef Harag <32jojo32@gmail.com>
2020-09-28 17:01:32 +0200

    Fetch real statistics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,6 +8,7 @@ import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import '@oacore/design/lib/index.css'
 import 'main/global.css'
+import apiRequest from '../api'
 
 import Main from 'main'
 import { Sentry } from 'utils/sentry'
@@ -19,30 +20,6 @@ process.on('unhandledRejection', (err) => {
 process.on('uncaughtException', (err) => {
   Sentry.captureException(err)
 })
-
-export async function getStaticProps({ res }) {
-  res.setHeader(
-    'Content-Security-Policy',
-    [
-      // consider everything from these two domains as a safe
-      "default-src 'self' *.core.ac.uk core.ac.uk",
-      // in development there are attached inline scripts
-      // (probably from hot reload or some Next.JS magic)
-      `script-src 'self' *.google-analytics.com ${
-        process.env.NODE_ENV !== 'production' ? "'unsafe-inline'" : ''
-      }`,
-      `style-src 'self' ${
-        process.env.NODE_ENV !== 'production' ? "'unsafe-inline'" : ''
-      }`,
-      // google analytics may transport info via image
-      // https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#transport
-      "img-src 'self' *.core.ac.uk core.ac.uk data: 'self' *.google-analytics.com",
-      `connect-src 'self' *.core.ac.uk core.ac.uk sentry.io *.google-analytics.com ${
-        process.env.NODE_ENV !== 'production' ? 'localhost:* 127.0.0.1:*' : ''
-      }`,
-    ].join(';')
-  )
-}
 
 class App extends NextApp {
   // eslint-disable-next-line class-methods-use-this
@@ -57,13 +34,54 @@ class App extends NextApp {
   }
 
   render() {
-    const { Component, pageProps } = this.props
-
+    const { Component, pageProps, statistics } = this.props
     return (
       <Main>
-        <Component {...pageProps} />
+        <Component {...pageProps} statistics={statistics} />
       </Main>
     )
+  }
+}
+
+let statistics = {
+  timestamp: Date.now(),
+  data: null,
+}
+
+// TODO: Replace with getStaticProps once this is solved
+//       https://github.com/vercel/next.js/discussions/10949
+App.getInitialProps = async ({ ctx: { res } }) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    [
+      // consider everything from these two domains as a safe
+      "default-src 'self' *.core.ac.uk core.ac.uk",
+      // in development there are attached inline scripts
+      // (probably from hot reload or some Next.JS magic)
+      `script-src 'self' *.google-analytics.com ${
+        process.env.NODE_ENV !== 'production' ? "'unsafe-inline'" : ''
+      }`,
+      "style-src 'self' 'unsafe-inline'",
+      // google analytics may transport info via image
+      // https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#transport
+      "img-src 'self' *.core.ac.uk core.ac.uk data: 'self' *.google-analytics.com maps.wikimedia.org",
+      `connect-src 'self' *.core.ac.uk core.ac.uk sentry.io *.google-analytics.com ${
+        process.env.NODE_ENV !== 'production' ? 'localhost:* 127.0.0.1:*' : ''
+      }`,
+    ].join(';')
+  )
+
+  if (
+    !statistics.data ||
+    // fetch new stats if they are 1 day old
+    (Date.now() - statistics.timestamp) / (1000 * 3600 * 24) > 1
+  ) {
+    const { data } = await apiRequest('/statistics')
+    statistics = data
+  }
+
+  return {
+    statistics,
   }
 }
 
