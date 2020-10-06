e4f8f71586e64392b96ea2370671584e21b15175
Jozef Harag <32jojo32@gmail.com>
2020-10-06 16:05:23 +0200

    Add data provider details page layout

---
+++ pages/data-providers/[data-provider-id].jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import DataProvider from 'store/data-provider'
+import { useStore, observe } from 'store'
+import DataProviderTemplate from 'templates/data-provider'
+
+export async function getServerSideProps({ query }) {
+  const id = query['data-provider-id']
+
+  const outputs = await DataProvider.loadOutputs({
+    id,
+  })
+  const metadata = await DataProvider.loadMetadata({
+    id,
+  })
+
+  return {
+    props: {
+      initialState: {
+        dataProvider: {
+          outputs,
+          id: query['data-provider-id'],
+          metadata,
+        },
+      },
+    },
+  }
+}
+
+const DataProviderPage = observe(() => {
+  const { dataProvider } = useStore()
+  return (
+    <DataProviderTemplate
+      outputs={dataProvider.outputs}
+      metadata={dataProvider.metadata}
+    />
+  )
+})
+
+export default DataProviderPage
