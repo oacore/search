fb90d7e6980af3fcd8dbd8441c303f7891572dcd
Jozef Harag <32jojo32@gmail.com>
2020-10-06 16:05:23 +0200

    Connect search to API

--- templates/data-provider/index.jsx
+++ templates/data-provider/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import { SearchResult } from '@oacore/design'
 
-import Result from './result'
 import DataProviderOutputsSearch from './search'
 import ClaimCard from './claim-card'
 import MapCard from './map-card'
@@ -8,27 +8,75 @@ import styles from './styles.module.css'
 
 import Search from 'modules/search-layout'
 
-const DataProviderTemplate = ({ outputs, metadata }) => (
-  <Search className={styles.layout}>
-    <Search.Main>
-      <span className={styles.titleBox}>
-        <span>{metadata.institution}</span>
-        <h1 className={styles.title}>{metadata.name}</h1>
-      </span>
+const DataProviderTemplate = ({
+  outputs,
+  metadata,
+  query,
+  setQuery,
+  loadSuggestions,
+  loadOutputs,
+  loading,
+}) => {
+  const placeholders = Array.from(Array(10).keys())
 
-      <DataProviderOutputsSearch
-        onQueryChanged={() => {}}
-        className={styles.search}
-      />
-      {outputs.map((o) => (
-        <Result key={o.id} {...o} />
-      ))}
-    </Search.Main>
-    <Search.Sidebar tag="aside">
-      <MapCard metadata={metadata} />
-      <ClaimCard name={metadata.name} />
-    </Search.Sidebar>
-  </Search>
-)
+  return (
+    <Search className={styles.layout}>
+      <Search.Main>
+        <span className={styles.titleBox}>
+          <span>{metadata.institution}</span>
+          <h1 className={styles.title}>{metadata.name}</h1>
+        </span>
+
+        <DataProviderOutputsSearch
+          initQuery={query}
+          onQueryChanged={(q) => {
+            setQuery(q)
+            loadOutputs()
+          }}
+          className={styles.search}
+          loadSuggestions={loadSuggestions}
+        />
+        {loading
+          ? placeholders.map((key) => (
+              <SearchResult
+                id={String(key)}
+                aria-busy="true"
+                tag={Search.Result}
+              >
+                <p data-line-count="2" />
+              </SearchResult>
+            ))
+          : outputs.map(({ id, abstract, title, author, year, link }) => {
+              const fullTextLink = link.find((l) => l.type === 'download')?.url
+              const metadataLink = link.find((l) => l.type === 'display')?.url
+              return (
+                <SearchResult
+                  key={id}
+                  id={id}
+                  tag={Search.Result}
+                  data={{
+                    title,
+                    author,
+                    publicationDate: year,
+                    thumbnailUrl: `//core.ac.uk/image/${id}/medium`,
+                    metadataLink,
+                    fullTextLink,
+                  }}
+                >
+                  {abstract}
+                </SearchResult>
+              )
+            })}
+        {outputs.length === 0 && (
+          <div className={styles.noResultsFind}>No results found</div>
+        )}
+      </Search.Main>
+      <Search.Sidebar tag="aside">
+        <MapCard metadata={metadata} />
+        <ClaimCard name={metadata.name} />
+      </Search.Sidebar>
+    </Search>
+  )
+}
 
 export default DataProviderTemplate
