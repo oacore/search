eb5ee54831aa322d3fc009de81a962f0bc5fd1bb
Jozef Harag <32jojo32@gmail.com>
2020-11-16 12:18:35 +0100

    All rebase fixes

--- modules/search-layout/sidebar.jsx
+++ modules/search-layout/sidebar.jsx
@@ -4,10 +4,7 @@ import { classNames } from '@oacore/design/lib/utils'
 import styles from './styles.module.css'
 
 const Sidebar = ({ children, className, ...restProps }) => (
-  <div
-    className={classNames.use(styles.sidebar, className)}
-    {...restProps}
-  >
+  <div className={classNames.use(styles.sidebar, className)} {...restProps}>
     {children}
   </div>
 )
