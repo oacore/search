e2f2a103d2741a954d2dc4e3d203875b8321686b
Viktor Yakubiv <viktor@yakubiv.com>
2021-01-18 15:24:12 +0200

    Add loading reporting to the App, Main and Layout (#176)
    Adds fixed LoadingBar to the top of the page that appears
    based on the router state: from the `routeChangeStart` to
    either `routeChangeComplete` or `routeChangeError`.

--- modules/layout/index.jsx
+++ modules/layout/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { LoadingBar } from '@oacore/design'
 
 import Container from './container'
 import AppBar from './app-bar'
 import styles from './styles.module.css'
 import Footer from './footer'
 
-const Layout = ({ children }) => (
+const Layout = ({ children, loading }) => (
   <Container>
+    {loading && <LoadingBar fixed />}
     <AppBar />
     <main className={styles.main}>{children}</main>
     <Footer />
