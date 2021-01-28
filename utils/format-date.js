7b3abed5abf90e9cdd790ed0a2ba60da24480dd7
Viktor Yakubiv <viktor@yakubiv.com>
2021-01-28 16:33:46 +0200

    Add formatDate utility function

---
+++ utils/format-date.js
@@ -0,0 +1,20 @@
+const dateTimeFormatCache = new Map()
+
+const formatDate = (date, options = {}) => {
+  const stringOptions = JSON.stringify(options)
+  let dateTimeFormat = dateTimeFormatCache.get(stringOptions)
+  if (!dateTimeFormat) {
+    dateTimeFormat = new Intl.DateTimeFormat('en-GB', options)
+    dateTimeFormatCache.set(stringOptions, dateTimeFormat)
+  }
+
+  try {
+    return dateTimeFormat.format(new Date(date))
+  } catch (error) {
+    if (process.env.NODE_ENV === 'development')
+      console.error('Date in invalid format', date, error)
+    return date
+  }
+}
+
+export default formatDate
