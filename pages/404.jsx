2cba6bee3348e13b117569d7324dc866b3ef4871
Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
2021-08-02 13:27:58 +0300

    Core 3939 create new 404 page (#205)
    * Create 404 error page
    
    * Improve logic of error page(
    
    * Improve styles of error page
    
    * Fixe lint error in error page
    
    * Fixe bug with error page building
    
    * Change error displayed text

---
+++ pages/404.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+
+import Error404 from 'templates/error'
+import { fetchMetadata } from 'api/outputs'
+
+// pages/_error is only rendered in case there is an error
+// so it can't use getServerSideProps as it has to be rendered
+// immediately when an error happens.
+
+const Error = () => {
+  const [errorStatus, setErrorStatus] = useState()
+  const router = useRouter()
+  const { id } = router.query
+
+  useEffect(async () => {
+    try {
+      await fetchMetadata(id)
+    } catch (error) {
+      setErrorStatus(error.status)
+    }
+  }, [])
+
+  return <Error404 articleId={id} status={errorStatus} />
+}
+
+export default Error
