7878fadbb2c914a6617c0cbdf23edddf3c80e384
vkot91 <k.voskoboinik99@gmail.com>
2021-09-01 11:22:03 +0300

    Connect v3 api to the data-providers

--- api/data-provider.js
+++ api/data-provider.js
@@ -6,7 +6,7 @@ const API_V3_URL = process.env.API_URL.replace('/internal', '/v3')
 const requestV3 = (url, ...args) => request(`${API_V3_URL}${url}`, ...args)
 
 const fetchMetadata = async (id) => {
-  const { data } = await request(`/data-providers/${id}`)
+  const { data } = await requestV3(`/data-providers/${id}`)
   return data
 }
 
