070e01121616c1e0a6067eb0bd09b9b7010597b8
Valeriy Budko <valerij.budko@gmail.com>
2021-10-20 15:20:58 +0300

    Core 4004 restore the oai and last updated date field (#226)
    * CORE-4004
    
    Copy clipboard OAI implement
    
    * Style copy button
    
    * Fix eslint
    
    * Add OAI and date style
    
    * Popover style
    
    * Copy OAI id to clip board

--- templates/output/thumbnail-card.jsx
+++ templates/output/thumbnail-card.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import { Card } from '@oacore/design'
+import React, { useRef, useState, useReducer } from 'react'
 import { classNames } from '@oacore/design/lib/utils'
 import filesize from 'filesize'
+import { Icon, Button, Card, Popover } from '@oacore/design'
 
 import styles from './thumbnail-card.module.css'
 
+import formatDate from 'utils/format-date'
+
 const DISABLED_STATUS = 'disabled'
+
+const initialTooltipState = {
+  isTooltipShown: false,
+  tooltipText: 'Copy OAI to your clipboard',
+}
+
+const tooltipReducer = (state, action) => {
+  switch (action) {
+    case 'hide':
+      return initialTooltipState
+    case 'copied':
+      return { ...state, tooltipText: 'Copied!' }
+    case 'show':
+      return { ...initialTooltipState, isTooltipShown: true }
+    default:
+      throw new Error('Received unknown action in tooltipReducer')
+  }
+}
 
 const FullTextThumbnail = ({
   data: {
     title,
     size: fileSize,
     type: fileType,
+    identifiers,
+    createdDate,
     updatedDate,
     sourceFulltextUrls,
     fulltextStatus,
@@ -22,50 +44,127 @@ const FullTextThumbnail = ({
   href,
   className,
   ...passProps
-}) => (
-  <Card
-    id={id}
-    className={classNames.use(styles.container).join(className)}
-    {...passProps}
-  >
-    <h2 className={styles.title} id={`${id}-title`}>
-      Full text
-    </h2>
+}) => {
+  const textRef = useRef(null)
+  const [copySuccess, setCopySuccess] = useState('')
 
-    <OutputCard
-      tag={fulltextStatus === DISABLED_STATUS ? 'div' : 'a'}
-      href={href}
+  // const value = useContext(MyContext)
+
+  const [{ tooltipText, isTooltipShown }, dispatchTooltip] = useReducer(
+    tooltipReducer,
+    initialTooltipState
+  )
+
+  const copyToClipboard = () => {
+    if (
+      !document.queryCommandSupported &&
+      !document.queryCommandSupported('copy')
+    )
+      return
+
+    const copyText = textRef.current.textContent
+
+    navigator.clipboard.writeText(copyText).then(
+      () => {
+        // eslint-disable-next-line no-console
+        console.log(`Copying to clipboard: ${copyText}`)
+        setCopySuccess(true)
+      },
+      (err) => {
+        console.error('Async: Could not copy text: ', err)
+      }
+    )
+  }
+
+  let lastTimeUpdated = null
+  if (createdDate) lastTimeUpdated = createdDate
+  if (updatedDate) lastTimeUpdated = updatedDate
+
+  let oaiArray = []
+  if (identifiers && identifiers.oai) oaiArray = identifiers.oai.split(':')
+
+  return (
+    <Card
       id={id}
-      src={src}
-      alt={alt}
-      title={title}
-      fileType={fileType}
-      fileSize={fileSize}
-      fulltextStatus={fulltextStatus}
-    />
+      className={classNames.use(styles.container).join(className)}
+      {...passProps}
+    >
+      <h2 className={styles.title} id={`${id}-title`}>
+        Full text
+      </h2>
 
-    {(updatedDate || sourceFulltextUrls) && (
-      <p className={styles.body}>
-        {updatedDate && (
-          <Card.Description className={styles.description} tag="span">
-            Last time updated on {updatedDate}
-          </Card.Description>
-        )}
-        {sourceFulltextUrls && (
-          <Card.Description className={styles.descriptionLink} tag="span">
-            <a
-              href={sourceFulltextUrls}
-              aria-labelledby={`${id}-downloaded-from-title`}
-              aria-describedby={`${id}-downloaded-from-body`}
-            >
-              View original full text link
-            </a>
-          </Card.Description>
-        )}
-      </p>
-    )}
-  </Card>
-)
+      <OutputCard
+        tag={fulltextStatus === DISABLED_STATUS ? 'div' : 'a'}
+        href={href}
+        id={id}
+        src={src}
+        alt={alt}
+        title={title}
+        fileType={fileType}
+        fileSize={fileSize}
+        fulltextStatus={fulltextStatus}
+      />
+
+      {(identifiers || lastTimeUpdated || sourceFulltextUrls) && (
+        <p className={styles.body}>
+          {oaiArray.length === 3 && (
+            <Card.Description className={styles.descriptionLink} tag="span">
+              <span ref={textRef} className={styles.hidden}>
+                {oaiArray[2]}
+              </span>
+              {oaiArray[0]}
+              <span className={styles.detach}>:</span>
+              {oaiArray[1]}
+              <span className={styles.detach}>:</span>
+              {oaiArray[2]}
+              <Popover
+                placement="top"
+                content={tooltipText}
+                visible={isTooltipShown}
+                className={styles.popover}
+              >
+                <Button
+                  type="button"
+                  className={
+                    copySuccess ? styles.copyButtonSuccess : styles.copyButton
+                  }
+                  onClick={copyToClipboard}
+                  onMouseEnter={() => dispatchTooltip('show')}
+                  onMouseLeave={() => dispatchTooltip('hide')}
+                >
+                  <Icon src="#content-copy" />
+                </Button>
+              </Popover>
+            </Card.Description>
+          )}
+
+          {lastTimeUpdated && (
+            <Card.Description className={styles.descriptionDate} tag="span">
+              Last time updated on{' '}
+              {formatDate(new Date(lastTimeUpdated), {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </Card.Description>
+          )}
+
+          {sourceFulltextUrls && (
+            <Card.Description className={styles.description} tag="span">
+              <a
+                href={sourceFulltextUrls}
+                aria-labelledby={`${id}-downloaded-from-title`}
+                aria-describedby={`${id}-downloaded-from-body`}
+              >
+                View original full text link
+              </a>
+            </Card.Description>
+          )}
+        </p>
+      )}
+    </Card>
+  )
+}
 
 const OutputCard = ({
   tag: Tag = 'a',
