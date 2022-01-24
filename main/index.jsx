9da572c8bc493b2df79124b3b4e418974bee246e
Kostiantyn Voskoboinik <50716463+vkot91@users.noreply.github.com>
2022-01-24 11:31:27 +0200

    Move cookie provider to _app component (#254)

--- main/index.jsx
+++ main/index.jsx
@@ -1,28 +1,25 @@
 import React from 'react'
-import {
-  DesignProvider,
-  CookiesProvider,
-  Cookies as CookiesPopup,
-} from '@oacore/design'
+import { DesignProvider, Cookies as CookiesPopup } from '@oacore/design'
 
 import Head from './head'
 
 import Layout from 'modules/layout'
 import { useInitStore, StoreProvider } from 'store'
+import useAnalytics from 'hooks/use-analytics'
 
 const Main = ({ children, initialState, loading, isSearchPage }) => {
   const store = useInitStore(initialState)
 
+  useAnalytics()
+
   return (
     <StoreProvider store={store}>
       <DesignProvider>
-        <CookiesProvider>
-          <Head />
-          <Layout isSearchPage={isSearchPage} loading={loading}>
-            {children}
-          </Layout>
-          <CookiesPopup />
-        </CookiesProvider>
+        <Head />
+        <Layout isSearchPage={isSearchPage} loading={loading}>
+          {children}
+        </Layout>
+        <CookiesPopup />
       </DesignProvider>
     </StoreProvider>
   )
