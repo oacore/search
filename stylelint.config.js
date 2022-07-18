07aa567bb695bebc2bbdf9f5bc3687e435c855be
vkot91 <k.voskoboinik99@gmail.com>
2022-07-18 14:17:35 +0300

    Update style config path

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,1 +1,8 @@
-module.exports = require('@oacore/stylelint-config-base')
+const { resolve } = require('path')
+
+const basePath = resolve(__dirname, 'node_modules')
+const groupSelectors = `${basePath}/@oacore/stylelint-config-base`
+
+module.exports = {
+  extends: groupSelectors,
+}
