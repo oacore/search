95969a1ce66d097931c55c14ccf180146283a148
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2025-01-06 17:24:14 +0400

    Core 5087 missing full text label (#366)
    * CORE-5087: fix label issue
    
    * CORE-5087: fix label issue

--- templates/search/results.jsx
+++ templates/search/results.jsx
@@ -51,12 +51,7 @@ const Results = ({ works, searchId }) =>
         searchId: innerSearchId,
         id: innerId,
       }) => {
-        if (
-          innerFullTextLink == null &&
-          innerDownloadLink == null &&
-          innerModifiedReaderLink == null
-        )
-          return null
+        if (innerFullTextLink) return innerFullTextLink
         if (
           (innerFullTextLink && innerFullTextLink.includes('core')) ||
           (innerDownloadLink && innerDownloadLink.includes('core')) ||
@@ -70,8 +65,22 @@ const Results = ({ works, searchId }) =>
           )
         }
         if (innerDownloadLink) return innerDownloadLink
-        return innerFullTextLink
+        if (
+          innerFullTextLink == null &&
+          innerDownloadLink == null &&
+          innerModifiedReaderLink == null
+        )
+          return null
+        return null
       }
+
+      const fullTextLinkResolved = renderFullTextLink({
+        fullTextLink,
+        downloadLink,
+        modifiedReaderLink,
+        searchId,
+        id,
+      })
 
       return (
         <SearchResult
@@ -92,13 +101,7 @@ const Results = ({ works, searchId }) =>
             metadataLink:
               generateMetadataLink(metadataLink, searchId, id) ||
               generateMetadataLink(displayLink, searchId, id),
-            fullTextLink: renderFullTextLink({
-              fullTextLink,
-              downloadLink,
-              modifiedReaderLink,
-              searchId,
-              id,
-            }),
+            fullTextLink: fullTextLinkResolved,
             dataProviders: dataProviders || [],
             isRecommended: memberType?.billing_type === 'sustaining',
           }}
