6d7242451b35b8291b765577c016ba84523cb6a9
mcancellieri <mcancellieri@users.noreply.github.com>
2025-09-16 12:10:24 +0100

    update icons.svg loaction

--- design.config.js
+++ design.config.js
@@ -33,7 +33,7 @@ const config = {
 
   output: {
     path: path.join(__dirname, 'public/design'),
-    publicPath: '/design',
+    publicPath: '/static/search',
     icons: {
       files: 'icons',
       sprite: 'icons.svg',
