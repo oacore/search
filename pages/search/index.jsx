c6012d53b48b483152484733be82b9bb23a820d4
ekachxaidze98 <ekachxaidze2@gmail.com>
2026-07-27 16:49:02 +0400

    Revert "TimeoutUp"
    This reverts commit f56faa6cde48c24b6b3b7f1022e3ad28262b9eba.

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -28,7 +28,6 @@ const serializeSearchError = (error) => ({
   request: error?.request ?? null,
   code: error?.code ?? null,
   stack: error?.stack,
-  errorFull: error,
 })
 
 export const getServerSideProps = async ({ query: searchParams }) => {
